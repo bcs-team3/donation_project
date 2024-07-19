0b4b0bcccde16730f2fb381c611a1ec87d5014c8
starlash7 <yang92sw@gmail.com>
2024-07-19 11:34:16 +0900

    first commit

--- vite/src/pages/Home.tsx
+++ vite/src/pages/Home.tsx
@@ -1,14 +1,7 @@
 import { FC } from "react";
 
-
 const Home: FC = () => {
-
-  return (
-        <div className="bg-red-200">
-            HOME
-        </div>
-
-  );
+  return <div className="bg-red-200">HOME22222</div>;
 };
 
 export default Home;
